48b27d5ec3d82f717c1a7428db2cc0d306465385
Albert <ishengomaalbert@gmail.com>
2022-10-20 19:42:38 +0300

    Interface Implementation by a Class

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,37 @@
 //Used to define structure of an Object
-interface Person {
+interface Greetable {
   name: string;
   age: number;
   greet(phrase: string): void;
 }
 
-let person1: Person;
 
+//Class Implementation
+class Person implements Greetable{
+    
+    constructor(public name:string, public age:number, public gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender
+
+    }
+    greet(phrase: string){
+        console.log(phrase + " " + this.name);
+    };
+
+    otherMethod(){
+        console.log('hello world');
+        
+    }
+}
+
+let person1: Greetable;
+
+person1 = new Person('John',23, 'Male')
+person1.greet('Hello')
+
+
+//Object Implementation
 person1 = {
   name: "James Bond",
   age: 36,
@@ -15,5 +40,3 @@ person1 = {
   },
 };
 
-
-person1.greet('Hello')
