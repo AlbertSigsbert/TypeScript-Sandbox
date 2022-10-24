07cae45abb429ff73c6be36d84a004b3b4d24ee4
Albert <ishengomaalbert@gmail.com>
2022-10-24 15:11:57 +0300

    Function Overloads

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,21 @@ const errorBag: ErrorContainer = {
   email: "Not a valid email",
   username: "Username must start with a character",
 };
+
+//FUNCTION OVERLOADS
+type Combinable = number | string;
+
+
+function add(a: string, b: string):string;
+function add(a: number, b:number): number;
+
+function add(a: Combinable, b: Combinable) {
+  //Type Guard (Common JS Types)
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add('Albert', 'Sigsbert');
+console.log(result.split(''));
