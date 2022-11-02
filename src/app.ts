30d94bc5337cd6c61d5489846f4c82932035ac9b
Albert <ishengomaalbert@gmail.com>
2022-11-02 11:23:08 +0300

    Generic Constraints

--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,14 @@
 // const mergedObj = merge({name:'John'}, {age:20});
 
 
+// Generic Constraints
+function merge<T extends object,U extends object>(objA: T, objB: U){
+  return Object.assign(objA,objB);
+}
+
+const mergedObj = merge({name:'John', hobbies:['football', 'movies']}, {age:20});
+console.log(mergedObj);
+
+
+
 
