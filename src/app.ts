0fe70a998abd6452b94bd8afd2ab653732d70794
Albert <ishengomaalbert@gmail.com>
2022-11-08 20:58:06 +0300

    The keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,20 @@
 
 
 // Generic Constraints
-function merge<T extends object,U extends object>(objA: T, objB: U){
-  return Object.assign(objA,objB);
+// function merge<T extends object,U extends object>(objA: T, objB: U){
+//   return Object.assign(objA,objB);
+// }
+
+// const mergedObj = merge({name:'John', hobbies:['football', 'movies']}, {age:20});
+// console.log(mergedObj);
+
+
+//The keyof Constraint
+function test<T extends object, U extends keyof T>(obj:T, key:U){
+  return 'Value: '+ obj[key];
 }
 
-const mergedObj = merge({name:'John', hobbies:['football', 'movies']}, {age:20});
-console.log(mergedObj);
-
-
+const value = test({name:'John'}, 'name');
+console.log(value);
 
 
