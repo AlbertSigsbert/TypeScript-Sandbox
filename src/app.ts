664f13d975d5df90c3d93b3f85e31352cc550180
Albert <ishengomaalbert@gmail.com>
2022-11-17 12:48:04 +0300

    Accessor, Method & Param decorators

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@
 //Decorator Factory
 // function Logger(logString: string) {
 //   console.log('LOGGER FACTORY');
-  
+
 //   return function (constructor: Function) {
 //     console.log('LOGGER DECORATOR');
 //     console.log(logString);
@@ -18,10 +18,10 @@
 //Decorators as templates
 // function withTemplate(template:string, elementId:string){
 //   console.log('TEMPLATE FACTORY');
-  
+
 //   return function (constructor: any){
 //     console.log('TEMPLATE DECORATOR');
-    
+
 //     const el = document.getElementById(elementId);
 //     const p = new constructor();
 //     if(el){
@@ -45,19 +45,59 @@
 // const person = new Person();
 // console.log(person);
 
-
 //Property Decorators
-function Log(target:any, propertyName:string | Symbol){
-  console.log('PROERTY DECORATOR');
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("PROPERTY DECORATOR");
   console.log(target, propertyName);
 }
 
-class Product{
+//Accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("ACCESSOR DECORATOR");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
 
+//Method decorator
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("METHOD DECORATOR");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+//Parameter decorator
+function Log4(target: any, name: string | Symbol, position:number) {
+  console.log("PARAM DECORATOR");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
   @Log
-  title:string;
+  title: string;
+  private _price: number;
 
-  constructor(t:string){
+  constructor(t: string, p: number) {
     this.title = t;
+    this._price = p;
+  }
+
+  //A setter
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Price must be positive");
+    }
+  }
+
+  //A method
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
   }
 }
